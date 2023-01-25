d1c70a571932e1ab27350b78df5e4b4d5c583e89
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-25 15:35:08 +1300

    chore: test tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello World.</div>;
+  return <h1 className="text-3xl font-bold underline">Hello World.</h1>;
 };
 
 export default App;
