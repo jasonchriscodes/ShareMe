2bcded19ec53cf682db3261cd6bb8f6728951492
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-26 09:42:12 +1300

    chore: add logo

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -18,7 +18,11 @@ const Login = () => {
           muted
           autoPlay
         />
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay"></div>
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="200px" alt="logo" />
+          </div>
+        </div>
       </div>
     </div>
   );
