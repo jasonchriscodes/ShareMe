51f5e190ec17d58c44f2f107a91d29c74be130cb
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-25 14:44:35 +1300

    chore: add schemas path

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,3 +1,7 @@
 import user from './user'
+import pin from './pin'
+import comments from './comments'
+import postedBy from './postedBy'
+import save from './save'
 
-export const schemaTypes = [user]
+export const schemaTypes = [user, pin, comments, postedBy, save]
