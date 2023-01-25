83915e7c99af3069a96455ae7eebc0df0dda6f92
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-01-26 10:13:16 +1300

    chore: style sign in button

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -26,7 +26,12 @@ const Login = () => {
             <GoogleLogin
               clientId=""
               render={(renderProps) => (
-                <button className="bg-mainColor" type="button">
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
                   <FcGoogle className="mr-4" />
                   Sign in with google
                 </button>
