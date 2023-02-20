07f4ebdfd2353b453c578a8e895a523fbeb55e9a
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-02-21 08:31:14 +1300

    chore: add Sidebar categories

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import { RihomeFill } from "react-icons/ri";
+import { RiHomeFill } from "react-icons/ri";
 import { IoIosArrowForward } from "react-icons/io";
 import logo from "../assets/logo.png";
+
+const isNotActiveStyle =
+  "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
+const isActiveStyle =
+  "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
+
+const categories = [
+  { name: "Animals" },
+  { name: "Wallpapers" },
+  { name: "Photography" },
+  { name: "Gaming" },
+  { name: "Coding" },
+  { name: "Other" },
+];
 
 const Sidebar = (user, closeToggle) => {
   const handleCloseSidebar = () => {
@@ -18,6 +32,33 @@ const Sidebar = (user, closeToggle) => {
         >
           <img src={logo} alt="logo" className="w-full" />
         </Link>
+        <div className="flex flex-col gap-5">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+            onClick={handleCloseSidebar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">
+            Discover categories
+          </h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </div>
   );
