a083ca16447dd22c3ae35efe73bb23d6d6ee9a00
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-02-25 12:13:04 +1300

    chore: add loading spinner

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  if (loading)
+    return <Spinner message="We are adding new ideas to your feed!" />;
 
-export default Feed
+  return <div>Feed</div>;
+};
+
+export default Feed;
