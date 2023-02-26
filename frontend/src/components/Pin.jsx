475126cb2256203ec2d34fe291f177fc55423104
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-02-27 08:26:17 +1300

    refactor: fetchUser()

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -5,11 +5,13 @@ import { MdDownloadForOffline } from "react-icons/md";
 import { AiTwotoneDelete } from "react-icons/ai";
 import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
 import { urlFor, client } from "../client";
+import { fetchUser } from "../utils/fetchUser";
 
 const Pin = ({ pin: { postedBy, image, _id, destination } }) => {
   const [postHovered, setPostHovered] = useState(false);
   const [savingPost, setSavingPost] = useState(false);
   const navigate = useNavigate();
+  const user = fetchUser();
 
   return (
     <div className="m-2">
