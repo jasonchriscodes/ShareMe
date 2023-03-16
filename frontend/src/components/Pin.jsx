51d19ba5d3603918450b442ccda5bac5d0e2c06c
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-16 16:24:18 +1300

    fix: user id

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -12,7 +12,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   const navigate = useNavigate();
   const user = fetchUser();
 
-  const alreadySaved = !!(save?.filter((item) => item?.postedBy?._id === user?.googleId))?.length;
+  const alreadySaved = !!(save?.filter((item) => item?.postedBy?._id === user?.sub))?.length;
 
   const savePin = (id) => {
     if (!alreadySaved) {
