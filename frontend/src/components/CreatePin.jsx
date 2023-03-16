4591fb7981ef607d18b9112d92cbab1430ce03f9
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-16 23:51:28 +1300

    chore: add file upload condition if have no image

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -19,6 +19,34 @@ const CreatePin = ({ user }) => {
 
   const navigate = useNavigate();
 
+  const uploadImage = (e) => {
+    const { type, name } = e.target.files[0];
+    if (
+      type === "image/png" ||
+      type === "image/svg" ||
+      type === "image/jpeg" ||
+      type === "image/gif" ||
+      type === "image/tiff"
+    ) {
+      setWrongImageType(false);
+      setLoading(true);
+      client.assets
+        .upload("image", e.target.files[0], {
+          contentType: type,
+          filename: name,
+        })
+        .then((document) => {
+          setImageAsset(document);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log("Image upload error: ", error);
+        });
+    } else {
+      setWrongImageType(true);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
       {fields && (
@@ -31,6 +59,29 @@ const CreatePin = ({ user }) => {
           <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
             {loading && <Spinner />}
             {wrongImageType && <p>Wrong image type</p>}
+            {!imageAsset ? (
+              <label>
+                <div className="flex flex-col items-center justify-center h-full">
+                  <div className="flex flex-col justify-center items-center">
+                    <p className="font-bold text-2xl">
+                      <AiOutlineCloudUpload />
+                    </p>
+                    <p className="text-lg">Click to upload</p>
+                  </div>
+                  <p className="mt-32 text-gray-400">
+                    Use high quality JPG, SVG, PNG, JPEG, GIF less than 20 MB
+                  </p>
+                </div>
+                <input
+                  type="file"
+                  name="upload-image"
+                  onChange={uploadImage}
+                  className="w-0 h-0"
+                />
+              </label>
+            ) : (
+              <p>something else</p>
+            )}
           </div>
         </div>
       </div>
