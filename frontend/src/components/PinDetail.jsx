0a4b21d8f07a59a27e69715b40c30137580bc814
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-18 16:19:35 +1300

    chore: add pinDetail page

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { MdDownloadForOffline } from "react-icons/md";
+import { Link, useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import { clinet, urlFor } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import { pinDetailMorePinQuery, pinDetailQuery } from "../utils/data";
+import Spinner from "./Spinner";
 
-const PinDetail = () => {
-  return (
-    <div>PinDetail</div>
-  )
-}
+const PinDetail = ({ user }) => {
+  const [pins, setPins] = useState(null);
+  const [pinDetail, setPinDetail] = useState(null);
+  const [comment, setComment] = useState("");
+  const [addingComment, setAddingComment] = useState(false);
+  // ID...
+  const { pinId } = useParams();
 
-export default PinDetail
+  if (!pinDetail) return <Spinner message="Loading pin..." />;
+
+  return <div>PinDetail</div>;
+};
+
+export default PinDetail;
