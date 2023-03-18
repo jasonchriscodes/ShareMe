f729b34ef4dfb4fa577be6986c133b12ccc3dafc
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-19 03:07:53 +1300

    chore: add loading condition in search component

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MasonryLayout from "./MasonryLayout";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import Spinner from "./Spinner";
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div>
+      {loading && <Spinner message="Searching for pins..." />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl">No Pins found!</div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
