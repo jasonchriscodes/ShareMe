ed8a6621ca1e547a877ef3df6b7c7547e827b6cb
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-18 18:41:47 +1300

    chore: create add comment feature

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -15,6 +15,32 @@ const PinDetail = ({ user }) => {
   // ID...
   const { pinId } = useParams();
 
+  const addComment = () => {
+    if (comment) {
+      setAddingComment(true);
+
+      client
+        .patch(pinId)
+        .setIfMissing({ comments: [] })
+        .insert("after", "comments[-1]", [
+          {
+            comment,
+            _key: uuidv4(),
+            postedBy: {
+              _type: "postedBy",
+              _ref: user._id,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          fetchPinDetails();
+          setComment("");
+          setAddingComment(false);
+        });
+    }
+  };
+
   const fetchPinDetails = () => {
     let query = pinDetailQuery(pinId);
 
@@ -101,6 +127,29 @@ const PinDetail = ({ user }) => {
               </div>
             </div>
           ))}
+        </div>
+        <div className="flex flex-wrap mt-6 gap-3">
+          <Link to={`user-profile/${pinDetail.postedBy?._id}`}>
+            <img
+              className="w-10 h-10 rounded-full cursor-pointer"
+              src={pinDetail.postedBy?.image}
+              alt="user-profile"
+            />
+          </Link>
+          <input
+            type="text"
+            className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-grey-300"
+            placeholder="Add a comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button
+            type="button"
+            className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none"
+            onClick={addComment}
+          >
+            {addingComment ? "Posting the comment..." : "Post"}
+          </button>
         </div>
       </div>
     </div>
