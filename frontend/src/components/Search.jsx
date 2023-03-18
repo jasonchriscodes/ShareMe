525f535faeda8082c5bc97e807ffa943d5bae826
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-19 03:13:43 +1300

    chore: add search functionality

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -8,6 +8,23 @@ const Search = ({ searchTerm }) => {
   const [pins, setPins] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
   return (
     <div>
       {loading && <Spinner message="Searching for pins..." />}
